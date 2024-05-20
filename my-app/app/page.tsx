49e3b6cbe802a738e023bfc8ab0bd0ec7e13fabe
Vikas Kumar Sharma <svikaskumar23@gmail.com>
2024-05-20 14:55:49 +0530

    add timeline and region functionality in waveform

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,16 +1,11 @@
 import UploadAudio from '@/components/UploadAudio'
-import { FileContextProvider } from '@/contexts/fileContext'
 import React from 'react'
 
 const page = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-
-        <FileContextProvider>
-          <UploadAudio />
-        </FileContextProvider>
-
+        <UploadAudio />
       </div>
     </main>
   )
